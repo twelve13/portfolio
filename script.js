099e924656025b68d208395a83d70f18b338fbf5
Caroline Chen <carolinecaroline@gmail.com>
2017-05-29 12:14:42 -0400

    organize code

--- script.js
+++ script.js
@@ -1,104 +1,41 @@
-console.log("hello");
+//var nav = $(".nav");
+
+var aboutButton = $(".about-button");
+var about = $(".about");
+var aboutWrapper = $(".about-wrapper");
+var closeAbout = $(".close-about");
+
+var projectsButton = $(".projects-button");
 var projects = $(".projects");
 var projectsWrapper = $(".projects-wrapper");
 var projectsNav = $(".projects-nav");
-var about = $(".about");
 var projectListing = $("project-listing");
 var closeProjects = $(".close-projects");
-var aboutWrapper = $(".about-wrapper");
-var closeAbout = $(".close-about");
-var aboutButton = $(".about-button");
-var projectsButton = $(".projects-button");
+
 var contactButton = $(".contact-button");
-var nav = $(".nav");
-var title = $(".title");
 
+//var title = $(".title");
 
+var aboutListing = $(".about-listing");
+//var experience = $(".experience");
+var blurb = $(".blurb");
 var studii = $(".studii");
 var studiiBlurb = $(".studii-blurb")
 var endangered = $(".endangered");
 var endangeredBlurb = $(".endangered-blurb");
 var simon = $(".simon");
 var simonBlurb = $(".simon-blurb");
-var blurb = $(".blurb");
 var final= $(".final");
-var aboutListing = $(".about-listing");
-var experience = $(".experience");
+
 var nameLetter = $(".name-letter")
 
-var projectsClicked = false;
 var aboutClicked = false;
+var projectsClicked = false;
 
-
-projectsWrapper.hide();
-blurb.hide();
 aboutWrapper.hide();
 closeAbout.hide();
-
-
-
-studii.on("click", function(){
-	studii.css("color", "white");
-	endangered.css("color", "gray");
-	simon.css("color", "gray");
-	endangeredBlurb.hide();
-	simonBlurb.hide();
-	studiiBlurb.show();
-});
-
-endangered.on("click", function(){
-	endangered.css("color", "white");
-	studii.css("color", "gray");
-	simon.css("color", "gray");
-	studiiBlurb.hide();
-	simonBlurb.hide();
-	endangeredBlurb.show();
-})
-
-simon.on("click", function(){
-	simon.css("color", "white");
-	studii.css("color", "gray");
-	endangered.css("color", "gray");
-	studiiBlurb.hide();
-	endangeredBlurb.hide();
-	simonBlurb.show();
-})
-
-
-projectsButton.click(
-function() {
-	if (projectsClicked === false){
-
-	projects.animate({
-		height: "90vh",
-		width: "540px",
-		top: "-=310px"
-	}, 2000)
-	projectsWrapper.show();
-
-	// studiiBlurb.show();
-	projectListing.show();
-	projectsNav.show();
-	
-	projectsClicked = true;
-	aboutButton.hide();
-	contactButton.hide();
-
-}
-})
-
-	closeProjects.click(function(){
-		projects.animate({
-		height: "180px",
-		width: "32px",
-		top: "40px"
-	}, 2000)
-	projectsWrapper.hide();
-	
-	projectsClicked = false;
-	aboutButton.show();
-	contactButton.show();
-	});
+projectsWrapper.hide();
+blurb.hide();
 
 
 aboutButton.click(function(){
@@ -148,4 +85,72 @@ closeAbout.click(function(){
 	}, 2000)
 
 	aboutClicked = false;	
-	})
+});
+
+
+projectsButton.click(
+function() {
+	if (projectsClicked === false){
+
+	projects.animate({
+		height: "90vh",
+		width: "540px",
+		top: "-=310px"
+	}, 2000)
+	projectsWrapper.show();
+
+	// studiiBlurb.show();
+	projectListing.show();
+	projectsNav.show();
+	
+	projectsClicked = true;
+	aboutButton.hide();
+	contactButton.hide();
+
+}
+});
+
+closeProjects.click(function(){
+		projects.animate({
+		height: "180px",
+		width: "32px",
+		top: "40px"
+	}, 2000)
+	projectsWrapper.hide();
+	
+	projectsClicked = false;
+	aboutButton.show();
+	contactButton.show();
+});
+
+studii.on("click", function(){
+	studii.css("color", "white");
+	endangered.css("color", "gray");
+	simon.css("color", "gray");
+	endangeredBlurb.hide();
+	simonBlurb.hide();
+	studiiBlurb.show();
+});
+
+endangered.on("click", function(){
+	endangered.css("color", "white");
+	studii.css("color", "gray");
+	simon.css("color", "gray");
+	studiiBlurb.hide();
+	simonBlurb.hide();
+	endangeredBlurb.show();
+})
+
+simon.on("click", function(){
+	simon.css("color", "white");
+	studii.css("color", "gray");
+	endangered.css("color", "gray");
+	studiiBlurb.hide();
+	endangeredBlurb.hide();
+	simonBlurb.show();
+})
+
+
+
+
+
